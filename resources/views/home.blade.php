d1502111bf058b1d9413d7b072da8782fb8187e9
MikePio <michelepiopilla@gmail.com>
2023-06-08 19:54:40 +0200

    Completed design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,40 @@
 
 @section('content')
 
-  <h1>Home main</h1>
+<div class="container d-flex flex-column align-items-center justify-content-center" style="min-height: calc(100vh - 90px - 150px);">
 
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Azienda</th>
+        <th scope="col">Stazione di partenza</th>
+        <th scope="col">Stazione di arrivo</th>
+        <th scope="col">Orario di partenza</th>
+        <th scope="col">Orario di arrivo</th>
+        <th scope="col">Codice Treno</th>
+        <th scope="col">Numero Carrozze</th>
+        <th scope="col">In orario</th>
+        <th scope="col">Cancellato</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td>
+          <td>7</td>
+          <td>8</td>
+          <td>9</td>
+        </tr>
+
+    </tbody>
+  </table>
+
+</div>
 
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,26 @@
-<footer>
+<footer class="bg-dark text-center text-white">
+  <!-- Grid container -->
+  <div class="container p-4 pb-0">
+      <!-- Section: Social media -->
+      <section class="mb-4">
+        <!-- Instagram -->
+        <a class="btn btn-outline-light btn-floating m-1 rounded-circle" href="https://www.instagram.com/michelepiopilla/" role="button"><i class="fab fa-instagram"></i></a>
 
-<h3>Footer</h3>
+        <!-- Linkedin -->
+        <a class="btn btn-outline-light btn-floating m-1 rounded-circle" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+
+        <!-- Github -->
+        <a class="btn btn-outline-light btn-floating m-1 rounded-circle" href="https://github.com/MikePio" role="button"><i class="fab fa-github"></i></a>
+
+      </section>
+      <!-- Section: Social media -->
+  </div>
+
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2023 By
+      <a class="text-white" href="https://github.com/MikePio">@MikePio</a>
+  </div>
+
 
 </footer>
+
