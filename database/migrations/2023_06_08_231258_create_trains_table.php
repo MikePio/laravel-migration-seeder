09e1665fde88750ff17df6b937478d93c7de97c8
MikePio <michelepiopilla@gmail.com>
2023-06-09 04:59:52 +0200

    Created and cycled the data subsequently inserted in the db table

--- database/migrations/2023_06_08_231258_create_trains_table.php
+++ database/migrations/2023_06_08_231258_create_trains_table.php
@@ -18,16 +18,16 @@ return new class extends Migration
           $table->id();
           $table->timestamps();
           // aggiunti successivamente
-          $table->string('slug', 50)->unique();
-          $table->string('agency', 30);
+          $table->string('agency', 60);
           $table->string('departure_station', 30);
           $table->string('arrival_station', 30);
-          $table->string('departure_time', 5);
-          $table->string('arrival_time', 5);
+          $table->string('departure_time', 8);
+          $table->string('arrival_time', 8);
           $table->string('train_code', 5)->unique();
           $table->tinyInteger('number_of_carriages')->unsigned()->default(0);
           $table->boolean('in_time')->nullable();
           $table->boolean('deleted')->nullable();
+          $table->string('slug', 50)->unique();
 
 
         });
--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,6 +5,13 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+//* aggiungere il model
+use App\Models\Train;
+
+//* aggiungere faker
+use Faker\Generator as Faker;
+//* illuminate per generare gli slug
+use Illuminate\Support\Str;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -13,8 +20,45 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+      //! nel caso fosse esistito un file trains.php con i dati all'interno (come un db) avrei dovuto scrivere
+      //! $train = config('trains');
+
+      $agencies = [
+        'Trenitalia',
+        'SNCF (Société Nationale des Chemins de Fer Français)',
+        'Deutsche Bahn',
+        'Amtrak',
+        'JR East (East Japan Railway Company)',
+        'Renfe Operadora',
+        'China Railway Corporation',
+        'Indian Railways',
+        'Russian Railways',
+        'Canadian National Railway'
+    ];
+
+      //* il numero massimo impostato nel ciclo è il numero di treni generati con tutti i dati assegnati in basso
+      for ($i = 0; $i < 5 ; $i ++) {
+        $new_train = new Train();
+        $new_train->agency = $faker->randomElement($agencies);
+        $new_train->departure_station = $faker->word();
+        $new_train->arrival_station = $faker->word();
+        $new_train->departure_time = $faker->time();
+        $new_train->arrival_time = $faker->time();
+        $new_train->train_code = implode('', [$faker->randomLetter(), $faker->randomLetter(), $faker->randomNumber(3)]);
+        $new_train->number_of_carriages = $faker->randomDigitNotNull();
+        $new_train->in_time = $faker->boolean();
+        $new_train->deleted = $faker->boolean();
+        $new_train->slug = Str::slug(implode(' ', [$new_train->departure_station, $new_train->arrival_station,  $new_train->train_code]), '-');
+
+        //! PER FARE UN CONTROLLO  dump($new_train);  + nel terminale: php artisan db:seed --class=TrainsTableSeeder
+        // dump($new_train);
+
+        //* per inviarli al database $new_train->save(); + nel terminale: php artisan db:seed --class=TrainsTableSeeder
+        $new_train->save();
+      }
+
+
     }
 }
